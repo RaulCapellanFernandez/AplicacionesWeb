831c0c714608dd7a19206c7d7d546f0c83c4ba64
Cp <raulcape.7@gmail.com>
2020-02-24 17:35:23 +0100

    modulos propios->Log

--- Curso-NodeJS/src/index.js
+++ Curso-NodeJS/src/index.js
@@ -1,7 +1,10 @@
 /*
 *Crear un servidor con el modulo HTTP
 */
-var http = require('http');
+var http = require('http');//Ejecutar JavaScript de lado del servidor
+var log = require('./modules/my-log');//Comillas simples y dobles para strings
+var consts = require('./utils/consts');
+var firebase = require('../libs/firebase');
 
 var server = http.createServer(function (request, response){
     if(request.url== "/"){
@@ -12,14 +15,22 @@ var server = http.createServer(function (request, response){
         response.writeHead(200, {'Content-Type': 'text/html'});//Todo fue correcto
         response.write('<html><body><p>Cartas</p></body></html>');//HTML de inicio que siempre va a mostrar
         response.end();
+    }else if(request.url == "/info"){
+        var result = log.info(request.url);
+        response.writeHead(200, {'Content-Type': 'text/html'});//Todo fue correcto
+        response.write(result);//HTML de inicio que siempre va a mostrar
+        response.end();
+    }else if(request.url == "/error"){
+        var result = log.error(request.url);
+        response.writeHead(200, {'Content-Type': 'text/html'});//Todo fue correcto
+        response.write(result);//HTML de inicio que siempre va a mostrar
+        response.end();
+    
     }else{
         response.writeHead(404, {'Content-Type': 'text/html'});//Todo fue correcto
         response.write('<html><body><p>Not Found</p></body></html>');//HTML de inicio que siempre va a mostrar
         response.end();
     }
-
-
-    
 });
 
 server.listen(4000);//Puerto en el que escucha el serrvidor
