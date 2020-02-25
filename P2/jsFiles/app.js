84ea21f6051eaf87d44885e56b4fd7b91ce20028
cp <cp@cps-MacBook-Pro.local>
2020-02-25 20:22:53 +0100

    Campos rellenados vue

---
+++ P2/jsFiles/app.js
@@ -0,0 +1,11 @@
+var vueApp = new Vue({
+    el: '#vueApp',
+    data: {
+        nombre: "Raul Capellan Fernandez",
+        edad: "18",
+        htmlCB: ["HTML1"],
+        picked: 'female',
+        color: '#5CA5BB',
+        fecha: '2017-07-04'
+    }
+})
